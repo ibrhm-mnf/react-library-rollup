8bd1024be5ec2bda78f3ca70b11ed0ad8ab0dfec
Ibrahim Manaf P <ibrahim.manaf@fingent.com>
2020-05-30 18:37:22 +0530

    core-js added and configured in babelrc file

--- src/components/DemoComponent.js
+++ src/components/DemoComponent.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const DemoComponent = () => {
+  const getData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const json = await response.json()
+    console.log(json, 'hi')
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return <div>Hi, Demo Component</div>
 }
 
